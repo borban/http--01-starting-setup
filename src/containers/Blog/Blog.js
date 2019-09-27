7208135bdd1ca42ae4e915fbcde3fd8faedfc11f
Ben Orban <benjamin.orban@ehi.com>
2019-09-27 10:47:48 -0500

    Lesson 193

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import axios from 'axios';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
 
   componentDidMount() {
@@ -22,10 +23,18 @@ class Blog extends Component {
       })
   }
 
+  postSelectedHandler = id => {
+    this.setState({selectedPostId: id})
+  };
+
   render () {
     const posts = this.state.posts.map(
       post => {
-        return <Post key={post.id} title={post.title} author={post.author}/>;
+        return <Post
+          key={post.id}
+          title={post.title}
+          author={post.author}
+          clicked={() => this.postSelectedHandler(post.id)}/>;
       }
     );
         return (
@@ -34,7 +43,7 @@ class Blog extends Component {
                   {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
